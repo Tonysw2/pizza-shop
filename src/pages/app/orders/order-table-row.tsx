7ec9b3eb28814668823071daa9edf8ffa6828ec7
Tonysw2 <anthonyrmayaa@gmail.com>
2024-01-15 14:32:34 -0300

    feat: add order details dialog

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,35 +1,51 @@
 import { ArrowRight, Search, X } from 'lucide-react'
 
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+
 import { Button } from '../../../components/ui/button'
 import { TableCell, TableRow } from '../../../components/ui/table'
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
+
       <TableCell className="font-mono text-xs font-medium">
         823hd9832gf924
       </TableCell>
+
       <TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
+
       <TableCell>
         <div className="flex items-center gap-2">
           <span className="h-2 w-2 rounded-full bg-slate-400" />
           <span className="font-medium text-foreground">Pendente</span>
         </div>
       </TableCell>
+
       <TableCell className="font-medium">Anthony Ribeiro</TableCell>
+
       <TableCell className="font-medium">R$ 149,90</TableCell>
+
       <TableCell>
         <Button variant="ghost" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
           Aprovar
         </Button>
       </TableCell>
+
       <TableCell>
         <Button variant="ghost" size="xs">
           <X className="mr-2 h-3 w-3" />
