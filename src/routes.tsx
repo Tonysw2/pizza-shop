aeae4fcee725677b3f5eef09447162ebeee32248
Tonysw2 <anthonyrmayaa@gmail.com>
2024-01-15 14:52:57 -0300

    feat: add order-related cards

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import { useRoutes } from 'react-router-dom'
 
 import { AppLayout } from '@/pages/_layouts/app'
 import { AuthLayout } from '@/pages/_layouts/auth'
-import { Dashboard } from '@/pages/app/dashboard'
+import { Dashboard } from '@/pages/app/dashboard/dashboard'
 import { SignIn } from '@/pages/auth/sign-in'
 
 import { Orders } from './pages/app/orders/orders'
