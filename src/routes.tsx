0888cbefe4e2e1181a70ed17f68f5d6b1292bd95
Anthony Ribeiro da Mata <anthonyrmataa@gmail.com>
2024-01-16 18:03:20 -0300

    refactor: remove unused imports

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, useRoutes } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 
 import { Dashboard } from '@/pages/app/dashboard/dashboard'
 import { SignIn } from '@/pages/auth/sign-in'
